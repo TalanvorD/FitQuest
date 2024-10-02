e5fd98ed73eb58315d77bb9121b623614e1d6a85
William <bzfishmaster94@gmail.com>
2024-10-01 22:30:15 -0400

    Made it so the header / footer disappears in login, also added files that are new for css

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -20,7 +20,7 @@ const Header = () => {
 
   return (
     <header id="header-bg" className="bg-primary text-light mb-4 py-3 flex-row align-center">
-      <div className="container flex-row justify-space-between-lg justify-center align-center">
+      <div id="header-content" className="container flex-row justify-space-between-lg justify-center align-center">
         <div>
           <Link className="text-light" to="/">
             <h1 className="m-0" id="header-font">Fit Quest</h1>
