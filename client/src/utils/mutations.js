c6ea7aca61a83b29f383ecba090028a0add582cf
TalanvorD <talanvor_divine@yahoo.com>
2024-10-03 19:11:41 -0500

    Update mutations.js

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -48,3 +48,36 @@ export const ADD_ACTIVITY = gql`
     }
   }
 `;
+
+//Delete this later
+export const ADD_THOUGHT = gql`
+  mutation addThought($thoughtText: String!) {
+    addThought(thoughtText: $thoughtText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+      }
+    }
+  }
+`;
+
+//Delete this later
+export const ADD_COMMENT = gql`
+  mutation addComment($thoughtId: ID!, $commentText: String!) {
+    addComment(thoughtId: $thoughtId, commentText: $commentText) {
+      _id
+      thoughtText
+      thoughtAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        createdAt
+      }
+    }
+  }
+`;
