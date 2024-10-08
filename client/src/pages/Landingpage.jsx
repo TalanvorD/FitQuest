bfc2b83a81894c5714aeffa0566aec563f4c84f3
William <bzfishmaster94@gmail.com>
2024-10-07 21:52:56 -0400

    Added landing page / some css design

---
+++ client/src/pages/Landingpage.jsx
@@ -0,0 +1,25 @@
+import '../index.css';
+import '../App.css';
+import { Link } from 'react-router-dom'; // Import Link
+
+const LandingPage = () => {
+
+return (
+    <main id="landing-main">   
+      <img id="landing-page-bg" src="../public/homepage-bg-2.webp"></img>
+      <div id="landing-content">
+        <h2>Welcome Traveler to <span id="fitquest-span">FitQuest!</span></h2>
+        <p>In a world where every day presents a choice between the mundane and the extraordinary, you stand at the crossroads of opportunity. 
+            Imagine a realm where your daily good habits transform into thrilling quests, where every small victory contributes to a grand adventure. 
+            This is the world of <strong>FitQuest</strong>—a captivating application designed to guide you on a transformative journey towards self-improvement.
+        </p>
+        <Link to="/login">
+            <button id="landingpage-btn">Let the journey begin!</button>
+        </Link>
+      </div>
+    </main>
+  );
+}
+
+export default LandingPage;
+
